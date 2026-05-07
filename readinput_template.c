e504eda1123d25b3aef168d24023f79252e8a695
Khang <chuvinhkhang.chukid@gmail.com>
2026-05-08 01:43:59 +0700

    Improve input reading and command handling
    Refactor input handling and command processing logic.

--- readinput_template.c
+++ readinput_template.c
@@ -8,20 +8,28 @@ int main(){
     //read input line
     char* line = (char*) calloc(50,sizeof(char));
     fgets(line,50,stdin);
-    line[strlen(line)-1] = '\0';
-
-    //command's space split
-    char* cmd = strtok(line," ");
-    char* cmd2 = strtok(NULL," ");
+      
+    // remove '\n'
+    line[strcspn(line, "\n")] = '\0';
 
     //loop's break condition
     if(line[0] == '*'){
       break;
     }
 
-    //algorithm....
-    if(strcmp(cmd,"") == 0){
+    //command's space split
+    char* cmd = strtok(line," ");
+    char* cmd2 = strtok(NULL," ");
 
+    // algorithm
+    if(strcmp(cmd, "insert") == 0){
+      // xử lý insert
+    }
+    else if(strcmp(cmd, "delete") == 0){
+      // xử lý delete
+    }
+    else if(strcmp(cmd, "print") == 0){
+      // xử lý print
     }
 
     free(line);
